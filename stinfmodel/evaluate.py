bfbba09cd74dc94072679ab22f494f1104648a11
Rahul G. Krishnan <rahulk90@gmail.com>
2016-06-16 03:11:59 -0400

    split files

--- stinfmodel/evaluate.py
+++ stinfmodel/evaluate.py
@@ -1,14 +1,19 @@
 
-def infer(self, dataset):
+from theano import config
+import numpy as np
+"""
+Functions for evaluating a DKF object
+"""
+def infer(dkf, dataset):
     """
                             Posterior Inference using recognition network
     """
     assert len(dataset.shape)==3,'Expecting 3D tensor for data' 
-    assert dataset.shape[2]==self.params['dim_observations'],'Data dim. not matching'
-    eps  = np.random.randn(dataset.shape[0], dataset.shape[1], self.params['dim_stochastic']).astype(config.floatX)
-    return self.posterior_inference(X=dataset.astype(config.floatX), eps=eps)
+    assert dataset.shape[2]==dkf.params['dim_observations'],'Data dim. not matching'
+    eps  = np.random.randn(dataset.shape[0], dataset.shape[1], dkf.params['dim_stochastic']).astype(config.floatX)
+    return dkf.posterior_inference(X=dataset.astype(config.floatX), eps=eps)
 
-def evaluateBound(self, dataset, mask, batch_size, actions=None, S=2, normalization = 'frame', additional={}):
+def evaluateBound(dkf, dataset, mask, batch_size, actions=None, S=2, normalization = 'frame', additional={}):
     """
                                     Evaluate bound on dataset
     """
@@ -27,7 +32,7 @@ def evaluateBound(self, dataset, mask, batch_size, actions=None, S=2, normalizat
         maxT    = int(np.max(M.sum(1)))
         X       = X[:,:maxT,:]
         M       = M[:,:maxT]
-        eps     = np.random.randn(X.shape[0],maxT,self.params['dim_stochastic']).astype(config.floatX)
+        eps     = np.random.randn(X.shape[0],maxT,dkf.params['dim_stochastic']).astype(config.floatX)
         if actions is not None:
             U   = actions[st_idx:end_idx,:maxT,:].astype(config.floatX)
         
@@ -35,14 +40,14 @@ def evaluateBound(self, dataset, mask, batch_size, actions=None, S=2, normalizat
         bound_sum, tsbn_bound_sum = 0, 0
         for s in range(S):
             if s%500==0:
-                self._p('Done '+str(s))
-            eps     = np.random.randn(X.shape[0],maxT,self.params['dim_stochastic']).astype(config.floatX)
+                dkf._p('Done '+str(s))
+            eps     = np.random.randn(X.shape[0],maxT,dkf.params['dim_stochastic']).astype(config.floatX)
             if actions is not None:
-                batch_vec= self.evaluate(X=X, M=M, eps=eps, U=U)
+                batch_vec= dkf.evaluate(X=X, M=M, eps=eps, U=U)
             else:
-                batch_vec= self.evaluate(X=X, M=M, eps=eps)
+                batch_vec= dkf.evaluate(X=X, M=M, eps=eps)
             if np.any(np.isnan(batch_vec)) or np.any(np.isinf(batch_vec)):
-                self._p('NaN detected during evaluation. Ignoring this sample')
+                dkf._p('NaN detected during evaluation. Ignoring this sample')
                 maxS -=1
                 continue
             else:
@@ -58,6 +63,85 @@ def evaluateBound(self, dataset, mask, batch_size, actions=None, S=2, normalizat
         assert False,'Invalid normalization specified'
         
     end_time   = time.time()
-    self._p(('(Evaluate) Validation Bound: %.4f [Took %.4f seconds], TSBN Bound: %.4f')%(bound,end_time-start_time,tsbn_bound))
+    dkf._p(('(Evaluate) Validation Bound: %.4f [Took %.4f seconds], TSBN Bound: %.4f')%(bound,end_time-start_time,tsbn_bound))
     additional['tsbn_bound'] = tsbn_bound
     return bound
+
+
+
+    
+def impSamplingNLL(dkf, dataset, mask, batch_size, actions=None, S = 2, normalization = 'frame'):
+    """
+                                Importance sampling based log likelihood
+    """
+    ll = 0
+    start_time = time.time()
+    N = dataset.shape[0]
+    for bnum,st_idx in enumerate(range(0,N,batch_size)):
+        end_idx = min(st_idx+batch_size, N)
+        X       = dataset[st_idx:end_idx,:,:].astype(config.floatX)
+        M       = mask[st_idx:end_idx,:].astype(config.floatX)
+        U       = None
+
+        #Reduce the dimensionality of the tensors based on the maximum size of the mask
+        maxT    = int(np.max(M.sum(1)))
+        X       = X[:,:maxT,:]
+        M       = M[:,:maxT]
+        eps     = np.random.randn(X.shape[0],maxT,dkf.params['dim_stochastic']).astype(config.floatX)
+        if actions is not None:
+            U   = actions[st_idx:end_idx,:maxT,:].astype(config.floatX)
+        maxS = S
+        lllist = []
+        for s in range(S):
+            if s%500==0:
+                dkf._p('Done '+str(s))
+            eps     = np.random.randn(X.shape[0],maxT,dkf.params['dim_stochastic']).astype(config.floatX)
+            if U is not None:
+                batch_vec = dkf.likelihood(X=X, M=M, eps=eps, U=U)
+            else:
+                batch_vec = dkf.likelihood(X=X, M=M, eps=eps)
+            if np.any(np.isnan(batch_vec)) or np.any(np.isinf(batch_vec)):
+                dkf._p('NaN detected during evaluation. Ignoring this sample')
+                maxS -=1
+                continue
+            else:
+                lllist.append(batch_vec)
+        ll  += dkf.meanSumExp(np.concatenate(lllist,axis=1), axis=1).sum()
+    if normalization=='frame':
+        ll /= float(mask.sum())
+    elif normalization=='sequence':
+        ll /= float(N)
+    else:
+        assert False,'Invalid normalization specified'
+
+    end_time   = time.time()
+    dkf._p(('(Evaluate w/ Imp. Sampling) Validation LL: %.4f [Took %.4f seconds]')%(ll,end_time-start_time))
+    return ll
+
+
+def sampleGaussian(dkf,mu,logsigmasq):
+        return mu + np.random.randn(*mu.shape)*np.exp(0.5*logsigmasq)
+
+def sample(dkf, nsamples=100, T=10, U= None, additional = {}):
+    """
+                                  Sample from Generative Model
+    """
+    if dkf.params['dim_actions']>0:
+        assert U is not None,'Specify U for sampling model conditioned on actions'
+    assert T>1, 'Sample atleast 2 timesteps'
+    #Initial sample
+    z      = np.random.randn(nsamples,1,dkf.params['dim_stochastic']).astype(config.floatX)
+    all_zs = [np.copy(z)]
+    additional['mu']     = []
+    additional['logcov'] = []
+    for t in range(T-1):
+        mu,logcov = dkf.transition_fxn(z)
+        z           = dkf.sampleGaussian(mu,logcov).astype(config.floatX)
+        all_zs.append(np.copy(z))
+        additional['mu'].append(np.copy(mu))
+        additional['logcov'].append(np.copy(logcov))
+    zvec = np.concatenate(all_zs,axis=1)
+    additional['mu']     = np.concatenate(additional['mu'], axis=1)
+    additional['logcov'] = np.concatenate(additional['logcov'], axis=1)
+    return dkf.emission_fxn(zvec), zvec
+    
--- stinfmodel/learning.py
+++ stinfmodel/learning.py
@@ -1,4 +1,147 @@
 """
-Functions for learning
+Functions for learning with a DKF object
 """
-import stinfmodels.
+import stinfmodels.evaluate as DKF_evaluate
+
+def learn(dkf, dataset, mask, epoch_start=0, epoch_end=1000, 
+          batch_size=200, shuffle=False,
+          savefreq=None, savefile = None, 
+          dataset_eval = None, mask_eval = None, 
+          replicate_K = None,
+          normalization = 'frame'):
+    """
+                                            Train DKF
+    """
+    assert not dkf.params['validate_only'],'cannot learn in validate only mode'
+    assert len(dataset.shape)==3,'Expecting 3D tensor for data'
+    assert dataset.shape[2]==dkf.params['dim_observations'],'Dim observations not valid'
+    N = dataset.shape[0]
+    idxlist   = range(N)
+    batchlist = np.split(idxlist, range(batch_size,N,batch_size))
+
+    bound_train_list,bound_valid_list,bound_tsbn_list = [],[],[]
+    p_norm, g_norm, opt_norm = None, None, None
+
+    #Lists used to track quantities
+    mu_list_train, cov_list_train, mu_list_valid, cov_list_valid, nll_valid_list = [],[],[],[],[]
+    current_lr = dkf.params['lr']
+
+    #Start of training loop
+    for epoch in range(epoch_start, epoch_end):
+        #Shuffle
+        if shuffle:
+            np.random.shuffle(idxlist)
+            batchlist = np.split(idxlist, range(batch_size,N,batch_size))
+        #Always shuffle order the batches are presented in
+        np.random.shuffle(batchlist)
+
+        start_time = time.time()
+        bound = 0
+        for bnum, batch_idx in enumerate(batchlist):
+            batch_idx  = batchlist[bnum]
+            X       = dataset[batch_idx,:,:].astype(config.floatX)
+            M       = mask[batch_idx,:].astype(config.floatX)
+            U       = None
+
+            #Tack on 0's if the matrix size (optimization->theano doesnt have to redefine matrices)
+            if X.shape[0]<batch_size:
+                Nremaining = int(batch_size-X.shape[0])
+                X   = np.concatenate([X,np.zeros((Nremaining,X.shape[1],X.shape[2]))],axis=0).astype(config.floatX)
+                M   = np.concatenate([M,np.zeros((Nremaining,X.shape[1]))],axis=0).astype(config.floatX)
+
+            #Reduce the dimensionality of the tensors based on the maximum size of the mask
+            maxT    = int(np.max(M.sum(1)))
+            X       = X[:,:maxT,:]
+            M       = M[:,:maxT]
+
+
+            eps     = np.random.randn(X.shape[0],maxT,dkf.params['dim_stochastic']).astype(config.floatX)
+
+            batch_bound, p_norm, g_norm, opt_norm, negCLL, KL, anneal = dkf.train_debug(X=X, M=M, eps=eps)
+
+            #Number of frames
+            M_sum = M.sum()
+
+            #Correction for replicating batch
+            if replicate_K is not None:
+                batch_bound, negCLL, KL = batch_bound/replicate_K, negCLL/replicate_K, KL/replicate_K, 
+                M_sum   = M_sum/replicate_K
+
+            #Update bound
+            bound  += batch_bound
+
+            ### Display ###
+            if bnum%10==0:
+                if normalization=='frame':
+                    bval = batch_bound/float(M_sum)
+                elif normalization=='sequence':
+                    bval = batch_bound/float(X.shape[0])
+                else:
+                    assert False,'Invalid normalization'
+                if p_norm is not None:
+                    dkf._p(('Bnum:%d, Batch Bound: %.4f, |w|: %.4f, |dw|: %.4f,|w_opt|:%.4f, -veCLL:%.4f, KL:%.4f: , anneal:%.4f')% (bnum,bval,p_norm, g_norm, opt_norm, negCLL, KL, anneal)) 
+                else:
+                    dkf._p(('Bnum:%d, Batch Bound: %.4f')%(bnum,bval)) 
+        if normalization=='frame':
+            bound /= float(mask.sum())
+        elif normalization=='sequence':
+            bound /= float(N)
+        else:
+            assert False,'Invalid normalization'
+        bound_train_list.append((epoch,bound))
+        end_time   = time.time()
+        dkf._p(('(Ep %d) Bound: %.4f , LR: %.4e[Took %.4f seconds] ')%(epoch, bound,current_lr, end_time-start_time))
+
+        #Save at intermediate stages
+        if savefreq is not None and epoch%savefreq==0:
+            assert savefile is not None, 'expecting savefile'
+            dkf._p(('Saving at epoch %d'%epoch))
+            dkf._saveModel(fname = savefile+'-EP'+str(epoch))
+
+            intermediate = {}
+            if dataset_eval is not None and mask_eval is not None:
+                tmpMap = {}
+                bound_valid_list.append(
+                    (epoch, DKF_evaluate.evaluateBound(dkf, dataset_eval, mask_eval, batch_size=batch_size, 
+                                              additional = tmpMap, normalization=normalization)))
+                bound_tsbn_list.append((epoch, tmpMap['tsbn_bound']))
+                nll_valid_list.append(DKF_evaluate.impSamplingNLL(dkf, dataset_eval, mask_eval, batch_size,
+                                                                  normalization=normalization))
+            intermediate['valid_bound'] = np.array(bound_valid_list)
+            intermediate['train_bound'] = np.array(bound_train_list)
+            intermediate['tsbn_bound']  = np.array(bound_tsbn_list)
+            intermediate['valid_nll']  = np.array(nll_valid_list)
+            saveHDF5(savefile+'-EP'+str(epoch)+'-stats.h5', intermediate)
+    #Final information to be collected
+    retMap = {}
+    retMap['train_bound']   = np.array(bound_train_list)
+    retMap['valid_bound']   = np.array(bound_valid_list)
+    retMap['tsbn_bound']   = np.array(bound_tsbn_list)
+    retMap['valid_nll']  = np.array(nll_valid_list)
+    if 'synthetic' in dkf.params['dataset']:
+        retMap['mu_posterior_train']  = np.concatenate(mu_list_train, axis=2)
+        retMap['cov_posterior_train'] = np.concatenate(cov_list_train, axis=2)
+        retMap['mu_posterior_valid']  = np.concatenate(mu_list_valid, axis=2)
+        retMap['cov_posterior_valid'] = np.concatenate(cov_list_valid, axis=2)
+    return retMap
+
+def _syntheticProc(dkf, dataset, dataset_eval):
+    """
+        Collect statistics on the synthetic dataset
+    """
+    allmus, alllogcov = [], []
+    for s in range(100):
+        _,mus, logcov = dkf.infer(dataset)
+        allmus.append(np.copy(mus))
+        alllogcov.append(np.copy(logcov))
+    allmus_v, alllogcov_v = [], []
+    for s in range(100):
+        _,mus, logcov = dkf.infer(np.copy(dataset_eval))
+        allmus_v.append(np.copy(mus))
+        alllogcov_v.append(np.copy(logcov))
+
+    mu_train = np.concatenate(allmus,axis=2).mean(2,keepdims=True)
+    cov_train= np.exp(np.concatenate(alllogcov,axis=2)).mean(2,keepdims=True)
+    mu_valid = np.concatenate(allmus_v,axis=2).mean(2,keepdims=True)
+    cov_valid= np.exp(np.concatenate(alllogcov_v,axis=2)).mean(2,keepdims=True)
+    return mu_train, cov_train, mu_valid, cov_valid
