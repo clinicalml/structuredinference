e8976fe89c7bc096067cd87b55d5d01686b61e23
Rahul G. Krishnan <rahulk90@gmail.com>
2017-03-16 20:31:37 -0400

    updated train

--- expt-polyphonic/train_dkf.py
+++ expt-polyphonic/train_dkf.py
@@ -25,8 +25,6 @@ import stinfmodel.evaluate as DKF_evaluate
 displayTime('import DKF',start_time, time.time())
 dkf    = None
 
-import ipdb;ipdb.set_trace()
-
 #Remove from params
 start_time = time.time()
 removeIfExists('./NOSUCHFILE')
